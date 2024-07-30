b9fb079210224d0c0ea6d7f02f208f70376d5718
github-floras-cc <github@floras.cc>
2024-07-30 13:08:35 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,308 @@
+/*
+***********************************
+testRTCAudio functions
+***********************************
+The following set of functions do the actual work to open an RTC AUDIO stream with whatever devices 
+are set as default (if any). This results in the mic being used to collect output for the playback
+device.
+*/
+
+function testRTCAudioCheckInputVolumeLevel() { //Function which reads the volume level of the RTC stream and changes a visual accordingly
+	navigator.mediaDevices.getUserMedia({ audio: true }).then(function(stream) {
+		audioContext = new AudioContext();
+		analyser = audioContext.createAnalyser();
+		microphone = audioContext.createMediaStreamSource(stream);
+		javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+		analyser.smoothingTimeConstant = 0.8;
+		analyser.fftSize = 1024;
+		microphone.connect(analyser);
+		analyser.connect(javascriptNode);
+		javascriptNode.connect(audioContext.destination);
+		javascriptNode.onaudioprocess = function() {
+			var array = new Uint8Array(analyser.frequencyBinCount);
+			analyser.getByteFrequencyData(array);
+			var values = 0;
+
+			var length = array.length;
+			for (var i = 0; i < length; i++) {
+			values += (array[i]);
+			}
+
+			var average = values / length;
+
+			if(Math.round(average)>15) {
+				console.log(Math.round(average));
+				document.getElementById("imgDeviceStatusInputLevel").style.height = Math.round(average)-5;
+				}
+			}
+		})
+	
+	.catch(function(err) {
+		return false;
+		});
+	}
+
+function testRTCAudio() { //Function which initiates audio devices, starts a stream and calls testRTCAudioHandleSuccess() to test your audio
+	testRTCAudioCheckInputVolumeLevel();
+	navigator.mediaDevices.getUserMedia(constraints).then(testRTCAudioHandleSuccess).catch(testRTCAudioHandleError);
+	document.getElementById('gum-local').play();
+	}
+
+function testRTCAudioHandleSuccess(stream) { //Function which runs the stream 
+	const audioTracks = stream.getAudioTracks();
+	console.log('Got stream with constraints:', constraints);
+	console.log('Using audio device: ' + audioTracks[0].label);
+	stream.oninactive = function() {
+		console.log('Stream ended');
+		};
+	window.stream = stream; // make variable available to browser console
+	audio.srcObject = stream;
+	}
+
+function testRTCAudioHandleError(error) { //Function which logs an error if testRTCAudio fails.
+	const errorMessage = 'navigator.MediaDevices.getUserMedia error: ' + error.message + ' ' + error.name;
+	console.log(errorMessage);
+	alert(errorMessage);
+	}
+
+
+/*
+***********************************
+UI / Logic functions
+***********************************
+Set of functions to support the steps/flows and holding the logic when to make options available
+*/
+
+function execStep(stepId) { //Function which does the visuals for each step + triggers the logic/tests
+	// Functions does all the logic for executing a step and doing the UI. 
+	// Returns: boolean
+	// Parameter 1: integer : Step number (1 2 or 3)
+	// Parameter 2: 
+
+	if (stepId < 2 || stepId > 3){
+		return false;
+		}
+
+	var activeStepDivName = "titleBox_step" + stepId;
+	var overlayToUnGrey = "stepOverlay" + stepId;
+	
+	// Reset everything, 
+	showDiv("titleBox_step1", 0);
+	showDiv("titleBox_step2", 0);
+	showDiv("titleBox_step3", 0);
+	document.getElementById('buttonStep1').className = 'buttonInactive';
+	document.getElementById('buttonStep1').disabled  = true;
+	document.getElementById('buttonStep2').className = 'buttonInactive';
+	document.getElementById('buttonStep2').disabled  = true;
+	document.getElementById('buttonStep3').className = 'buttonInactive';
+	document.getElementById('buttonStep3').disabled  = true;
+	document.getElementById('buttonStep3_1').className = 'buttonInactive';
+	document.getElementById('buttonStep3_1').disabled  = true;
+	
+	//Set the step active visually
+	showDiv(activeStepDivName, 1);
+	//Remove opacity from the relevant division
+	//document.getElementById(overlayToUnGrey).style.opacity = 100;
+	
+	if (stepId == 2){
+		document.getElementById('buttonStep2').className = 'buttonActive';
+		document.getElementById('buttonStep2').disabled  = false;
+		document.getElementById("stepOverlay2").style.opacity = 100;
+		document.getElementById("stepOverlay2").style.backgroundColor = "transparent";	
+		document.getElementById("stepOverlay3").style.opacity = 0.3;
+		document.getElementById("imgDeviceStatusInput").src = "media/icon_progress.gif";
+		document.getElementById("imgDeviceStatusOutput").src = "media/icon_progress.gif";
+		document.getElementById("stepWarningSettings").style.display = "none";
+		document.getElementById("stepWarningSettingsInfo").innerHTML = "";
+
+		document.getElementById('buttonStep2').disabled  = true;
+		document.getElementById('buttonStep2').className = 'buttonInactive';
+		getDefaultDevice("audioinput", "deviceInputDefaultInfoText");
+		getDefaultDevice("audiooutput", "deviceOutputDefaultInfoText");
+		triggerCheck();
+		}
+
+	if (stepId == 3){
+		document.getElementById('buttonStep3').className = 'buttonActive';
+		document.getElementById('buttonStep3').disabled  = false;
+		document.getElementById('buttonStep3_1').className = 'buttonActive';
+		document.getElementById('buttonStep3_1').disabled  = false;
+		document.getElementById("stepOverlay3").style.opacity = 100;
+		document.getElementById("stepOverlay3").style.backgroundColor = "transparent";	
+		document.getElementById('buttonStep2').className = 'buttonActive';
+		document.getElementById('buttonStep2').disabled  = false;
+		testRTCAudio();
+		}		
+
+	return true;
+	}
+
+function enableTestAudioStep() { //Function which enables step 3 audiotest irrespective of default devices 
+	// Functions enables the UI of step 3 to do the audio tsst when the checkbox is checked to be able to test audio despite not having a headset. 
+	// Returns: boolean
+	// Parameter 1: <none>
+	if (document.getElementById("enableTestAudioStep").checked == true) {
+		document.getElementById("stepOverlay3").style.opacity = 100;
+		document.getElementById("stepOverlay3").style.backgroundColor = "transparent";		
+		document.getElementById('buttonStep3').className = 'buttonActive';
+		document.getElementById('buttonStep3').disabled  = false;
+		document.getElementById('buttonStep3_1').className = 'buttonActive';
+		document.getElementById('buttonStep3_1').disabled  = false;
+		}
+	else {
+		document.getElementById("stepOverlay3").style.opacity = 0.3;
+		document.getElementById("stepOverlay3").style.backgroundColor = "#ffffff";		
+		document.getElementById('buttonStep3').className = 'buttonInactive';
+		document.getElementById('buttonStep3').disabled  = true;
+		document.getElementById('buttonStep3_1').className = 'buttonInactive';
+		document.getElementById('buttonStep3_1').disabled  = true;		
+		}
+	return true;
+	}
+
+function showIssue(deviceType, warningString) { //Function which renders the warning dialogue when wrong devices are detected and renders indication of issues
+	//Functions changes the visuals for an error situation. Called by getDefaultDevice.
+	// Returns: boolean, true is success
+	// Parameter 1: string : audioinput - or - audiooutput
+	// Parameter 2: string : The text to show in the warning dialogue indicating what is wrong
+	if (deviceType == "audioinput") { var image2update = "imgDeviceStatusInput"; }
+	if (deviceType == "audiooutput") { var image2update = "imgDeviceStatusOutput"; }
+	document.getElementById(image2update).src = "media/icon_nok.gif";
+	document.getElementById("stepWarningSettings").style.display = "block";
+	document.getElementById("stepWarningSettingsInfo").innerHTML = document.getElementById("stepWarningSettingsInfo").innerHTML + "<ul><li>" + warningString + "</li></ul>";	
+	return true;
+	}
+
+function showDiv(divId, toggleValue) { // Shows / hides a div to support UI execStep()
+	// Functions shows or hides a division
+	// Returns: boolean
+	// Parameter 1: string : division ID to show or hides
+	// Parameter 2: integer : 1=show, 2=hide  (defaults to 1)
+	var x = document.getElementById(divId);
+
+	if (toggleValue == 0) {
+		x.style.display = "none";
+		}
+	else {
+		x.style.display = "block";
+		}
+}
+
+function triggerCheck() { // Is called by execStep and iniates the checks after the devices are detemined with a timed delay (as getusermedia might take a few seconds)
+	// Functions to check the devices found and decide if audio test is advised.
+	// Returns: boolean
+	// Parameter 1: string : division ID to show or hides
+	// Parameter 2: integer : 1=show, 2=hide  (defaults to 1)
+	setTimeout(function(){
+		validateDefaultDevices("audioinput"); // Calls the checks the value for the input device.
+		}, 2000);
+	setTimeout(function(){
+		validateDefaultDevices("audiooutput"); // Calls the checks the value for the output device.
+		}, 3000);
+	setTimeout(function(){ 
+		// If no error was rendered then we can enable the audio test step!
+		if (document.getElementById("stepWarningSettingsInfo").innerHTML.trim() == "")
+			{
+			document.getElementById('buttonStep3').className = 'buttonActive';
+			document.getElementById('buttonStep3').disabled  = false;
+			document.getElementById('buttonStep3_1').className = 'buttonActive';
+			document.getElementById('buttonStep3_1').disabled  = false;
+			document.getElementById("stepOverlay3").style.opacity = 100;
+			document.getElementById("stepOverlay3").style.backgroundColor = "transparent";	
+			document.getElementById('buttonStep2').className = 'buttonActive';
+			document.getElementById('buttonStep2').disabled  = false;
+			}
+		else {
+			document.getElementById('buttonStep2').className = 'buttonActive';
+			document.getElementById('buttonStep2').disabled  = false;			
+			}
+		}, 3200);	
+	return true
+	}
+
+function validateDefaultDevices(deviceType) { // Logic to determine if there is an issue condition with the audio device selected.
+	// Functions to determine if there is an issue condition with the audio device selected and populates the warning dialogue
+	// Returns: boolean
+	// Parameter 1: string : audioinput - or - audiooutput to be checked.
+	if (deviceType == "audioinput") {
+		if (document.getElementById("deviceInputDefaultInfoText").innerHTML == "" ) {
+			showIssue("audioinput", "Geen microfoon gevonden om te gebruiken voor audio.");
+			return true;
+			}
+
+		str2check = document.getElementById("deviceInputDefaultInfoText").innerHTML;
+		if (str2check.includes("Realtek")) {
+			showIssue("audioinput", "De laptop microfoon wordt gebruikt.");
+			return true;
+			}
+
+		document.getElementById("imgDeviceStatusInput").src = "media/icon_ok.gif";
+		return true;
+	}
+
+	if (deviceType == "audiooutput") {
+		//alert( document.getElementById("deviceOutputDefaultInfoText").innerHTML );
+		if (document.getElementById("deviceOutputDefaultInfoText").innerHTML == "") {
+			showIssue("audiooutput", "Geen speaker apparaat gevonden om te gebruiken voor audio.");
+			return true;
+			}
+
+		str2check = document.getElementById("deviceOutputDefaultInfoText").innerHTML;
+		if (str2check.includes("Realtek")) {
+			showIssue("audiooutput", "De laptop speaker wordt gebruikt.");
+			return true;
+			}
+		if (str2check.includes("Display")) {
+			showIssue("audiooutput", "De monitor speaker wordt gebruikt.");
+			return true;
+			}
+	
+		document.getElementById("imgDeviceStatusOutput").src = "media/icon_ok.gif";
+		return true;		
+	}
+
+	return true;
+	}
+
+function getDefaultDevice(deviceType, spanToPopulate ) { //Functions collects the input or output audio device set up as "default" on the OS and writes the value to the designated span object
+	//Functions collects the input or output audio device set up as "default" and writes the value to the designated span object
+	// Returns: boolean, true is successfully written value to span, otherwise error
+	// Parameter 1: string : audioinput - or - audiooutput - or - video
+	// Parameter 2: string : the DOM id of the span to write the value to
+
+	document.getElementById(spanToPopulate).innerHTML = "";
+	const constraints = {'video': false, 'audio': true}
+	
+	if (!navigator.mediaDevices?.enumerateDevices) {
+		showIssue(deviceType, "Technical ERROR: EnumerateDevices() not supported.");
+		return false;
+		} 
+	else {
+		navigator.mediaDevices
+		.enumerateDevices()
+		.then((devices) => {
+			console.log(devices); 
+			devices.forEach((device) => {
+				if (`${device.label}` == "") {
+					//If label is Null this indicates that no permission was given in the browser for accessing the microfoon (could be a popup)
+					showIssue(deviceType, "Geen toestemming om audio (" + deviceType + ") te gebruiken. Zie instructies.");
+					return true;
+				}
+
+				if (`${device.kind}` == deviceType) {
+					var device_string = `${device.label}`;		
+					if (device_string.startsWith("Default") || device_string.startsWith("Standaard")) {
+						document.getElementById(spanToPopulate).innerHTML = `${device.label}`;
+						return true;
+						}
+					}
+				});
+			})
+		.catch((err) => {
+			console.error(`${err.name}: ${err.message}`);
+			return false;
+			});
+		}
+		
+	return true;
+	}
